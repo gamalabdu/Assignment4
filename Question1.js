2b576cc4095f5b9934d2e04e8d5ed8e27226657c
Andrew Conte <andrew-conte434>
2022-01-07 14:18:49 -0500

    added styling, ability to toggle buttons

--- Question1.js
+++ Question1.js
@@ -1,10 +1,24 @@
 let button1 = document.getElementById("button1")
 let button2 = document.getElementById("button2")
+let btnOn1 = false
+let btnOn2 = false
 
 button1.addEventListener("click", function () {
-    document.getElementById("button1Text").textContent = "I'm right!"
+    btnOn1 = !btnOn1
+    if (btnOn1){
+        document.getElementById("button1Text").textContent = "I'm right!"
+    } else {
+        document.getElementById("button1Text").textContent = ""
+    }
+    
 })
 
 button2.addEventListener("click", function () {
-    document.getElementById("button2Text").textContent = "No I'm right!"
+    btnOn2 = !btnOn2
+    if(btnOn2){
+        document.getElementById("button2Text").textContent = "No I'm right!"
+    } else {
+        document.getElementById("button2Text").textContent = ""
+    }
+    
 })
